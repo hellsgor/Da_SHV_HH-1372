e5daa16916b379d3bc3df801ef9becb7b80160fe
Igor Storchevoy <hellsgor@gmail.com>
2024-03-05 18:59:12 +0400

    feat(footer): green is ready

--- src/store.js
+++ src/store.js
@@ -6,7 +6,13 @@ export const store = {
       '/assets/images/blue_slide_4.jpg',
       '/assets/images/blue_slide_5.jpg',
     ],
-    green: [],
+    green: [
+      '/assets/images/green_slide_1.jpg',
+      '/assets/images/green_slide_2.jpg',
+      '/assets/images/green_slide_3.jpg',
+      '/assets/images/green_slide_4.jpg',
+      '/assets/images/green_slide_5.jpg',
+    ],
   },
 
   footerSliderItems: {
@@ -14,42 +20,48 @@ export const store = {
       iconID: 'wallet',
       title: 'Уверенность в завтрашнем дне',
       description: 'Достойная зарплата + премии, оформление  по ТК РФ с 1-го дня',
-      blue: true,
     },
 
     schedule: {
       iconID: 'schedule',
       title: 'Удобный сменный график',
       description: 'Полная и частичная занятость, ночные смены. Возможность совмещать работу с учебой',
-      blue: true,
     },
 
     gift: {
       iconID: 'gift',
       title: 'Корпоративная культура',
       description: 'Выездные мероприятия для сотрудников, фотосессии сотрудников, подарки на праздники',
-      blue: true,
     },
 
     student: {
       iconID: 'student',
       title: 'Нет опыта?  Обучим!',
       description: 'Оплачиваем обучение и стажировку + за каждым новым сотрудником закрепляется наставник',
-      blue: true,
     },
 
     rocket: {
       iconID: 'rocket',
       title: 'Карьерному росту  — ДА!',
       description: 'Бесплатные тренинги  и обучения, чтобы вы достигали новых вершин',
-      blue: true,
     },
 
     location: {
       iconID: 'location',
       title: 'Работай рядом  с домом',
       description: 'Более 200 супермаркетов  по Москве и ЦФО и это  только начало!',
-      blue: true,
+    },
+
+    car: {
+      iconID: 'car',
+      title: 'Корпоративный транспорт',
+      description: '',
+    },
+
+    coffee: {
+      iconID: 'coffee',
+      title: 'Оплата  питания',
+      description: '',
     },
   },
 }
